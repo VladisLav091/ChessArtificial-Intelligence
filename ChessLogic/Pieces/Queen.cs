f8f63b5ec21f12f9c13d86bf6f206d502013f8ff
VladisLav091 <vlad_bocharov_1991@mail.ru>
2024-03-31 12:43:40 +0300

    feat: Добавил логику подсчета ходов и передвижения для (queen, rook, bishop)

--- ChessLogic/Pieces/Queen.cs
+++ ChessLogic/Pieces/Queen.cs
@@ -10,7 +10,17 @@ namespace ChessLogic
     {
         public override PieceType Type => PieceType.Queen;
         public override Player Color { get; }
-
+        private static readonly Direction[] dirs = new Direction[]
+        {
+            Direction.East,
+            Direction.North,
+            Direction.South,
+            Direction.West,
+            Direction.NorthWest,
+            Direction.NorthEast,
+            Direction.SouthEast,
+            Direction.SouthWest,        
+        };
         public Queen(Player color)
         {
             Color = color;
@@ -20,6 +30,10 @@ namespace ChessLogic
             Queen copy = new Queen(Color);
             copy.HasMoved = HasMoved;
             return copy;
+        }
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            return MovePositionsInDirs(from, board, dirs).Select(to => new NormalMove(from, to));
         }
     }
 }
